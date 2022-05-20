bd3d9b23985af9d635e2403a848e3482b98fbb2e
jarikexe <jarikexe1@gmail.com>
2022-05-21 00:59:36 +0300

    add sockets

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,14 +1,35 @@
-import { View, StyleSheet, TextInput, Button } from "react-native";
+import {View, StyleSheet, TextInput, Button, Text} from "react-native";
+import {useEffect, useState} from "react";
+import {io} from "socket.io-client";
 
 export default function ChatScreen() {
+    //TODO: move to configs (or env var)
+    const socket = io("http://192.168.1.102:5000");
+    const [messages, setMessages] = useState([]);
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        socket.on("message", msg => {
+            setMessages([...messages, msg])
+        }
+    )});
+
+    const sendMessage = (message) => {
+        socket.emit('message', message);
+        setMessage('')
+    }
+
     return (
         <View style={styles.container}>
+            <View>
+                {messages.map(msg => <Text key={msg}>{msg}</Text>)}
+            </View>
             <View style={styles.inputContainer}>
                 <View style={styles.chatInputWrp}>
-                    <TextInput style={styles.chatInput} />
+                    <TextInput onChangeText={text => setMessage(text)} style={styles.chatInput} value={message} />
                 </View>
                 <View style={styles.sendButton}>
-                    <Button title="Send" />
+                    <Button title="Send" onPress={() => sendMessage(message)} />
                 </View>
             </View>
         </View>
